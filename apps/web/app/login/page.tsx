ba2e947f9ef06182d77b41e8d9d19f156448dfa3
Takahashi Yasunari <yasunarj@gmail.com>
2026-04-25 16:30:53 +0900

    add: Navigate to the login page

--- apps/web/app/login/page.tsx
+++ apps/web/app/login/page.tsx
@@ -4,9 +4,11 @@ import { Suspense } from "react";
 import LoginContent from "./LoginContent";
 
 const LoginPage = () => {
-  <Suspense fallback="読み込み中...">
-    <LoginContent />
-  </Suspense>
+  return (
+    <Suspense fallback="読み込み中...">
+      <LoginContent />
+    </Suspense>
+  );
 };
 
 export default LoginPage;
